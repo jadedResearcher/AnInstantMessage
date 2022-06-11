e39514454a689e418f85dd1f7e331f17bc34ae91
JR <jadedResearcher@gmail.com>
2022-06-11 13:01:18 -0400

    init

---
+++ story.js
@@ -0,0 +1,24 @@
+
+const me = "me";
+const you = "you";
+const thought = "[THOUGHT]"
+
+//note, for now, thoughts are not belonging to anyone
+//but if we wanna change that we'll need two types of thought markers
+
+//this is already there before the live content
+const backstory = `
+you: hey we're meeting at the disco right
+me: yeah sure disco sure is a real thing
+you: yeah thats how you can tell this is the past
+`;
+
+//this plays out live
+const story = `
+you: this is just a test
+me: yeah it really is
+you: but does it have a vibe?
+[THOUGHT] i sure hope it does
+[THOUGHT] please tell me it does
+you: i think it does
+`;
